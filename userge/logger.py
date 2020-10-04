e03665858ef6646614a104194fe9f10ed3111536
Udith <uaudith@gmail.com>
2020-10-04 11:40:25 +0530

    if logger not created 1

--- userge/logger.py
+++ userge/logger.py
@@ -14,6 +14,7 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
+os.makedirs(os.path.dirname("logs/userge.log"), exist_ok=True)
 if not os.path.isfile("logs/userge.log"):
     open("logs/userge.log", 'w+').close()
 logging.basicConfig(level=logging.INFO,
